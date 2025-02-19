b7ef11a9547bc0413f258723cdc91185be7241ca
Niels DESKTOP <niels.dejong@upcmail.nl>
2025-02-19 20:34:33 +0100

    Additional GNN layers

--- include/Nilib/ML/CNodes/Layer/DenseLayer.h
+++ include/Nilib/ML/CNodes/Layer/DenseLayer.h
@@ -11,7 +11,6 @@ namespace Nilib
     {
         CNode *input = nullptr, *W = nullptr;
 
-
         Linear(CNode *input, CNode *W)
             : input(input), W(W)
         {
@@ -94,6 +93,74 @@ namespace Nilib
         }
     };
 
+    // Highway GCN.
+    struct DenseGraphLayer2 : public CNode
+    {
+        CNode *A, *input, *input2, *W, *Wself;
+        bool owning = false;
+
+        DenseGraphLayer2(CNode *A, CNode *input, CNode *input2, CNode *W, CNode *Wself)
+            : A(A), input(input), input2(input2), W(W), Wself(Wself)
+        {
+        }
+
+        void evaluate()
+        {
+            A->evaluate();
+            input->evaluate();
+            input2->evaluate();
+            W->evaluate();
+            Wself->evaluate();
+
+            CORE_ASSERT(A->value.cols() == input->value.rows());
+            CORE_ASSERT(input->value.cols() == W->value.rows());
+            CORE_ASSERT(input2->value.cols() == Wself->value.rows());
+            this->value = 0.5 * A->value * input->value * W->value + 0.5 * input2->value * Wself->value;
+        }
+
+        void derive(Nilib::Matrixf const &seed)
+        {
+            A->derive(seed * transpose(input->value * W->value));
+            input->derive(transpose(A->value) * seed * transpose(W->value));
+            W->derive(transpose(A->value * input->value) * seed);
+            input2->derive(0.5 * seed * transpose(Wself->value));
+            Wself->derive(0.5 * transpose(input2->value) * seed);
+        }
+    };
+
+    struct OuterProductDecoder2 : public CNode
+    {
+        CNode *X, *W;
+
+        OuterProductDecoder2(CNode *X, CNode *W)
+            : X(X), W(W)
+        {
+        }
+
+        void evaluate()
+        {
+            X->evaluate();
+            W->evaluate();
+            CORE_ASSERT(X->value.cols() == W->value.rows());
+            CORE_ASSERT(X->value.cols() == W->value.cols());
+            float const scalefactor = X->value.rows() * X->value.rows();
+            this->value = X->value * W->value * Nilib::transpose(X->value) / scalefactor;
+            if (RNG::prob() < 0.00001)
+            {
+                LOG_DEBUG("Covar W", W->value, W->value.sum());
+            }
+        }
+
+        void derive(Nilib::Matrixf const &seed)
+        {
+            CORE_ASSERT(seed.cols() == X->value.rows());
+            CORE_ASSERT(X->value.rows() == seed.rows());
+            float const scalefactor = X->value.rows() * X->value.rows();
+            X->derive(seed * X->value * Nilib::transpose(W->value) / scalefactor + Nilib::transpose(seed) * X->value * W->value / scalefactor);
+            W->derive(Nilib::transpose(X->value) * seed * X->value / scalefactor);
+            // X->derive(Nilib::transpose(Nilib::transpose(X->value) * seed) + seed * Nilib::transpose(X->value));
+        }
+    };
     struct OuterProductDecoder : public CNode
     {
         CNode *X;
@@ -116,7 +183,7 @@ namespace Nilib
             CORE_ASSERT(X->value.rows() == seed.rows());
             float scalefactor = X->value.rows() * X->value.rows();
             X->derive(seed * X->value / scalefactor + Nilib::transpose(Nilib::transpose(X->value) * seed / scalefactor));
-            //X->derive(Nilib::transpose(Nilib::transpose(X->value) * seed) + seed * Nilib::transpose(X->value));
+            // X->derive(Nilib::transpose(Nilib::transpose(X->value) * seed) + seed * Nilib::transpose(X->value));
         }
     };
 
