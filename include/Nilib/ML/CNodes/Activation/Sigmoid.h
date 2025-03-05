fed079baca76b463dbcdba311d5e575e92512dc5
Niels DESKTOP <niels.dejong@upcmail.nl>
2025-03-05 20:58:05 +0100

    Added LinearSigmoid

--- include/Nilib/ML/CNodes/Activation/Sigmoid.h
+++ include/Nilib/ML/CNodes/Activation/Sigmoid.h
@@ -21,5 +21,33 @@ namespace Nilib
             return tmp * (1.0f - tmp);
         }
     };
+
+    struct SigmoidLinear : public Activation
+    {
+        SigmoidLinear(CNode *input)
+            : Activation(input,
+                         std::bind(&SigmoidLinear::sigmoidlinear, this, std::placeholders::_1),
+                         std::bind(&SigmoidLinear::sigmoidlinear_deriv, this, std::placeholders::_1)) {}
+
+        float sigmoidlinear(float const t)
+        {
+            if (t > 10.0f)
+                return t;
+            if (t < -6.0f)
+                return -0.0f;
+            return t / (1.0f + std::exp(-1.0f * t));
+        }
+
+        float sigmoidlinear_deriv(float const t)
+        {
+            if (t > 10.0f)
+                return 1.0f;
+            if (t < -6.0f)
+                return 0.0;
+            float const e_t = std::exp(-1.0f * t);
+            float const tmp = 1.0f + e_t;
+            return (tmp + t * e_t) / (tmp * tmp);
+        }
+    };
 }
 #endif
--- source/ML/CNodes/Input.cpp
+++ source/ML/CNodes/Input.cpp
@@ -4,7 +4,8 @@
 void Nilib::Input::set(Nilib::Matrixf const &value)
 {
     this->value = value;
-    this->partial = Nilib::Matrixf::zeros(value.rows(), value.cols());
+    if (this->partial.rows() != value.rows() || this->partial.cols() != value.cols())
+        this->partial = Nilib::Matrixf::zeros(value.rows(), value.cols());
 }
 
 Nilib::Input::Input(Nilib::Matrixf const &value) 
