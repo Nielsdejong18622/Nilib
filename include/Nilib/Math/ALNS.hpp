0f40ed4e6439307fe7f36e9f524577fa4da67eb5
NielsRUG <niels.de.jong@rug.nl>
2025-06-25 18:19:35 +0200

    (In)feasibility checks

--- include/Nilib/Math/ALNS.hpp
+++ include/Nilib/Math/ALNS.hpp
@@ -25,8 +25,12 @@ namespace Nilib
         struct ALNSparams
         {
             size_t max_iterations = 1000;
-            size_t max_seconds = 3;
+            size_t max_seconds = 5;
             float starting_temperature = 100.0f;
+
+            // Allow the ALNS solver to traverse infeasible solution space.
+            bool allow_infeasible = false;
+
             std::string history_filename = "";
         };
 
@@ -54,8 +58,8 @@ namespace Nilib
             {
                 ASSERT(operators.size() > 0, "ALNS::Solver requires atleast one operator!");
             }
-            	
-            // Sets initial solution. 
+
+            // Sets initial solution.
             void initial(Solution const &initial)
             {
                 bestfound = initial;
@@ -69,62 +73,85 @@ namespace Nilib
                 float best_obj = bestfound.objective();
                 float current_obj = best_obj;
                 float temperature = 1.0f;
-                LOG_PROGRESS("ALNS Solver starting with obj:", best_obj, "max_iterations:", params.max_iterations);
+
+                // Print starting information.
+                LOG_PROGRESS("ALNS::Solver Version 0.10 build [Win32]");
+                LOG_PROGRESS("User supplied", d_operators.size(), "operators");
+                LOG_PROGRESS("Optimizing a solution of size ", sizeof(Solution), "bytes");
+                LOG_PROGRESS("Initial solution objective:", best_obj);
                 if (params.history_filename != "")
-                    LOG_PROGRESS("ALNS Solver writing history to file", params.history_filename);
+                    LOG_PROGRESS("Writing history to file", params.history_filename);
 
                 // Cooling rate so that acceptance probability is ~0.01 at the end
                 float const coolingrate = std::pow(0.001f, 1.0f / params.max_iterations);
                 Nilib::Timer timer;
 
+                bool next_feasible = incumbent.feasible();
                 size_t iteration = 0;
                 for (; iteration < params.max_iterations; ++iteration)
                 {
+
                     if (timer.getSeconds() > params.max_seconds - 1)
                         break;
+
+                    // Call user defined callback if it exists!
+                    if (d_callback_iteration)
+                        d_callback_iteration(incumbent);
+
                     // Save current solution
                     auto previous = incumbent;
 
                     // Apply a random operator
-                    Operator op = d_operators[Nilib::RNG::index(d_operators.size())];
+                    size_t const selected_operator_idx = Nilib::RNG::index(d_operators.size());
+                    Operator op = d_operators[selected_operator_idx];
+
+                    bool current_feasible = next_feasible;
                     op(incumbent);
 
                     // Objective of the new solution
                     float next_obj = incumbent.objective();
+                    next_feasible = incumbent.feasible();
 
-                    // Delta cost (new - current)
-                    float delta = (next_obj - current_obj) / current_obj;
-
-                    bool accept = false;
-                    bool accept_with_anneal = false;
-                    if (delta < 0)
+                    // If we can not traverse infeasible solution space and the next solution is infeasible.
+                    if (!(!params.allow_infeasible && next_feasible == false))
                     {
+
+                        // Delta cost (new - current)
+                        float delta = (next_obj - current_obj) / current_obj;
+
+                        bool accept = false;
+                        bool accept_with_anneal = false;
+
                         // Better solution, always accept
-                        accept = true;
-                    }
-                    else
-                    {
-                        // Worse solution, accept with probability
-                        float prob = std::exp(-5.0f * delta / temperature);
-                        accept_with_anneal = (Nilib::RNG::prob() < prob);
-                    }
-
-                    if (accept || accept_with_anneal)
-                    {
-                        current_obj = next_obj;
-
-                        if (next_obj < best_obj)
+                        if (delta <= 0)
                         {
-                            if (d_callback_global_improvement)
-                                d_callback_global_improvement(incumbent);
-                            best_obj = next_obj;
-                            bestfound = incumbent;
+                            accept = true;
                         }
-                    }
-                    else
-                    {
-                        // Revert the change
-                        incumbent = bestfound;
+                        else
+                        {
+                            // Worse solution, accept with probability (simulated annealing).
+                            float prob = std::exp(-5.0f * delta / temperature);
+                            accept_with_anneal = (Nilib::RNG::prob() < prob);
+                        }
+
+                        if (accept || accept_with_anneal)
+                        {
+                            current_obj = next_obj;
+
+                            if (next_obj < best_obj)
+                            {
+                                if (d_callback_global_improvement)
+                                    d_callback_global_improvement(incumbent);
+                                best_obj = next_obj;
+                                bestfound = incumbent;
+                                LOG_PROGRESS("Found global improvement, objective:", best_obj);
+                            }
+                        }
+                        else
+                        {
+                            // Revert the change
+                            incumbent = bestfound;
+                        }
                     }
 
                     if (params.history_filename != "")
@@ -139,6 +166,13 @@ namespace Nilib
                 LOG_PROGRESS("ALNS solver performed", iteration, "iterations in", timer.getMilliseconds(), "ms.");
             }
 
+            // This userdefined callback is called at the start of each ALNS iteration.
+            void setIterationCallback(Callback callback)
+            {
+                d_callback_iteration = callback;
+            }
+
+            // This userdefined callback is called when a new bestfound solution is obtained.
             void setGlobalImprovementCallBack(Callback callback)
             {
                 d_callback_global_improvement = callback;
@@ -157,6 +191,6 @@ namespace Nilib
         };
 
     } // namespace ALNS
-}; //namespace Nilib
+}; // namespace Nilib
 
 #endif
