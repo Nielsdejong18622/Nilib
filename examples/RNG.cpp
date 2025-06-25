f2110ac2cb936ca31695e494ac24e5427cd2c3de
NielsRUG <niels.de.jong@rug.nl>
2025-06-25 15:19:58 +0200

    Added more tests and labels to RNG2

--- examples/RNG.cpp
+++ examples/RNG.cpp
@@ -1,6 +1,7 @@
 #include "Nilib/Math/RNG.hpp"
 #include "Nilib/Math/Stats.hpp"
 #include "Nilib/Core/Profiler.hpp"
+#include "Nilib/Core/Assert.hpp"
 
 using namespace Nilib;
 
@@ -10,7 +11,7 @@ int main()
     RNG::seed(127);
 
     // Generate a number between 0-1.
-    RNG::prob();
+    auto gen = RNG::prob();
 
     // Generate a lognormal number.
     RNG::lognormal(2.38, 0.683);
@@ -18,21 +19,40 @@ int main()
 
     for (size_t test = 0; test < 1; ++test)
     {
-        PROFILE_SCOPE("Testing probs");
+        PROFILE_SCOPE("Testing RNG::normal(20,0, 10.0)");
         Nilib::RunningStats probstat;
         for (size_t i = 0; i < 10'000'000; i++)
-            probstat.push(RNG::normal(0.0, 1.0));
-        LOG_INFO("Observed Probs:", probstat);
+            probstat.push(RNG::normal(20.0, 10.0));
+        LOG_INFO("Observed RNG::normal(20,0, 10.0)", probstat);
+    }
+
+    for (size_t test = 0; test < 1; ++test)
+    {
+        PROFILE_SCOPE("Testing RNG::index(10)");
+        Nilib::RunningStats probstat;
+        for (size_t i = 0; i < 10'000'000; i++)
+            probstat.push(RNG::index(10));
+        LOG_INFO("Observed RNG::index(10):", probstat);
     }
 
     Nilib::RNG::seed(127);
 
     for (size_t test = 0; test < 1; ++test)
     {
-        PROFILE_SCOPE("Testing probs");
+        PROFILE_SCOPE("Testing RNG::normal(0.0, 1.0)");
         Nilib::RunningStats probstat;
         for (size_t i = 0; i < 10'000'000; i++)
             probstat.push(Nilib::RNG::normal());
-        LOG_INFO("Observed Probs:", probstat);
+        LOG_INFO("Observed RNG::normal(0.0, 1.0):", probstat);
+    }
+
+    
+    for (size_t test = 0; test < 1; ++test)
+    {
+        PROFILE_SCOPE("Testing RNG::rand()");
+        Nilib::RunningStats probstat;
+        for (size_t i = 0; i < 10'000'000; i++)
+            probstat.push(Nilib::RNG::rand());
+        LOG_INFO("Observed RNG::rand():", probstat);
     }
 }
