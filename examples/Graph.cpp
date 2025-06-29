4b69e6657c0d63da9523e7cfb51900ddf3944cef
Niels DESKTOP <niels.dejong@upcmail.nl>
2025-06-29 22:33:50 +0200

    Restructure Graph lib

--- examples/Graph.cpp
+++ examples/Graph.cpp
@@ -1,53 +1,26 @@
-// #include "GraphLayout.hpp"
 #include "Nilib/Renderer/Window.hpp"
 #include "Nilib/Structures/Graph.hpp"
-#//include "LinAlg.hpp"
-
-struct Ndata
-{
-    int a;
-};
 
 int main()
 {
-    using namespace Nilib;
-    RNG::seed(127);
-    Window win(1024, 812, "Graph window!");
-    win.bindkey(std::bind(&Window::close, &win), GLFW_KEY_ESCAPE, GLFW_PRESS, 0);
+    using namespace Nilib::Graph;
 
-    // Tests.
-    std::vector<Ndata> locations(20, Ndata());
-    Graph<Ndata> erdos_reini;
+    // Step 1. Make a simple graph that stores relations. 
+    Graph<AdjacencyRep> graph;
 
-    LOG_DEBUG() << "G:" << erdos_reini << '\n';
-    erdos_reini.reserve(100);
-    LOG_DEBUG() << "G:" << erdos_reini << '\n';
-    erdos_reini = Graph<Ndata>::erdos_reini(254, 0.05);
+    graph.addNode(1);
+    graph.addNode(2);
+    graph.addEdge(1, 2);
 
-    LOG_DEBUG() << "G:" << erdos_reini << '\n';
 
-    LOG_DEBUG() << "Starting random walk\n";
-    for (Graph<Ndata>::node_id v : erdos_reini.randomWalk(0, 10))
-    {
-        LOG_DEBUG() << "Random walk:" << +v << '\n';
-    }
-    // Bounding box.
-    /*
-    GraphLayout layout = GraphLayout<Ndata>::random(erdos_reini, min, max);
-    win.bindkey(std::bind(&GraphLayout<Ndata>::update, &layout), GLFW_KEY_U, GLFW_PRESS, 0);
-    win.bindkey(std::bind(&Graph<Ndata>::removeEdge, &erdos_reini, RNG::uniform(0, 100), RNG::uniform(0, 100)), GLFW_KEY_P, GLFW_PRESS, 0);
-    win.bindkey(std::bind(&GraphLayout<Ndata>::update, &layout), GLFW_KEY_U, GLFW_REPEAT, 0);
+    // Step 2. Use graphmaps to create graphWithDatatype
+    
+    //DiGraphWithData<ListRepresentation, Location> g;
+    //DiGraphWithData<ListRepresentation, Location, Arc> g;
 
-    win.bindkey(std::bind(&GraphLayout<Ndata>::shuffle, &layout, erdos_reini, min, max), GLFW_KEY_R, GLFW_PRESS, 0);
-
-    while (!win.closed())
-    */
-    {
-        win.startScene();
-        // layout.draw(win);
-        // LOG_DEBUG("") << erdos_reini << '\n';
-        win.endScene();
-    }
+    // node_id n1 = g.addNode({1, 2});
+    // node_id dep = g.addNode({1, 2});
+    // edge_id e1 = g.addEdge(n1, dep);
 
     return 0;
 }
