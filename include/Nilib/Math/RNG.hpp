c09c86b905a64c65198c487e675a30676c2c0d2b
Niels DESKTOP <niels.dejong@upcmail.nl>
2025-09-26 16:20:23 +0200

    Add multiple random number streams

--- include/Nilib/Math/RNG.hpp
+++ include/Nilib/Math/RNG.hpp
@@ -13,7 +13,8 @@ namespace Nilib
 {
 
     // Uses wyhash64
-    class RNG
+    template <size_t stream = 0>
+    class RAN
     {
         static uint64_t state;
 
@@ -22,7 +23,8 @@ namespace Nilib
         {
             state = seed;
         }
-        // Seed with time.
+
+        // Seed with current time.
         static inline void seed()
         {
             auto now = std::chrono::high_resolution_clock::now();
@@ -52,20 +54,20 @@ namespace Nilib
         static inline float uniform(scalar const min, scalar const max)
         {
             ASSERT(max >= min, "Degenerate uniform!");
-            return min + RNG::prob() * (max - min);
+            return min + RAN<stream>::prob() * (max - min);
         }
 
         // Uniform random variable between [min, max]
         static inline int uniform(int const min, int const max)
         {
             ASSERT(max >= min, "Degenerate uniform!");
-            return min + RNG::index(max - min);
+            return min + RAN<stream>::index(max - min);
         }
         // Uniform random variable between [min, max]
         static inline size_t uniform(size_t const min, size_t const max)
         {
             ASSERT(max >= min, "Degenerate uniform!");
-            return min + RNG::index(max - min);
+            return min + RAN<stream>::index(max - min);
         }
 
         static inline uint64_t rand()
@@ -82,7 +84,7 @@ namespace Nilib
             for (auto &&w : weights)
                 total_weight += w;
 
-            auto idx = RNG::prob() * total_weight;
+            auto idx = RAN<stream>::prob() * total_weight;
 
             for (size_t i = 0; i < weights.size(); ++i)
             {
@@ -156,7 +158,7 @@ namespace Nilib
         static inline scalar weibull(scalar const scale, scalar const shape)
         {
             // TODO: Check !
-            return scale * std::pow(-1 * std::log(1 - RNG::prob()), 1.0f / shape);
+            return scale * std::pow(-1 * std::log(1 - RAN<stream>::prob()), 1.0f / shape);
         }
 
         // Shuffles container between [low, high), inplace.
@@ -168,7 +170,7 @@ namespace Nilib
             // Shuffle in place. (Fisher-Yates shuffle)
             for (size_t idx = low; idx < nobs - 1; ++idx)
             {
-                size_t jdx = RNG::index(nobs) + low;
+                size_t jdx = RAN<stream>::index(nobs) + low;
                 std::swap(container[idx], container[jdx]);
             }
         }
@@ -183,7 +185,7 @@ namespace Nilib
         static inline auto sample(Container const &container) -> decltype(container[0])
         {
             CORE_ASSERT(container.size() > 0);
-            return container[RNG::index(container.size())];
+            return container[RAN<stream>::index(container.size())];
         }
 
         template <typename Container, typename Weights>
@@ -193,7 +195,7 @@ namespace Nilib
             for (auto &&w : weights)
                 total_w += w;
 
-            return container[RNG::index(container.size())];
+            return container[RAN<stream>::index(container.size())];
         }
 
         // Uniform sample from an initializer list
@@ -202,103 +204,11 @@ namespace Nilib
         {
             CORE_ASSERT(list.size() > 0);
             std::vector<T> vec(list);
-            return vec[RNG::index(vec.size())];
+            return vec[RAN<stream>::index(vec.size())];
         }
     };
 
+    using RNG = RAN<0>;
 }
 
 #endif
-
-// #ifndef _RNG_H
-//  #define _RNG_H
-
-// #include <random>
-// #include <algorithm>
-
-// class RNG {
-
-// public:
-//     // Get the singleton instance
-//     static RNG& getInstance() {
-//         static RNG instance;
-//         return instance;
-//     }
-
-//     template<typename T>
-//     static T const &sample(std::vector<T> const &population) {
-//         return population[RNG::uniform<size_t>(0, population.size() - 1)];
-//     }
-
-//     static void seed(size_t const seed) {
-//         RNG::getInstance().gen = std::mt19937(seed);
-//     }
-
-//     // Generate a random probability between 0 and 1
-//     template<typename T = float>
-//     static T prob() {
-//         static_assert(std::is_arithmetic<T>::value,
-//                       "result_type must be a number type");
-//         std::uniform_real_distribution<T> dis(0.0, 1.0);
-//         return dis(RNG::getInstance().gen);
-//     }
-
-//     // Generate a uniform value in [min, max]
-//     template<typename T = double>
-//     static T uniform(T const min, T const max) {
-//         if constexpr (std::is_integral_v<T>)
-//         {
-//             std::uniform_int_distribution<T> dis(min, max);
-//             return dis(RNG::getInstance().gen);
-//         } else if constexpr (std::is_floating_point_v<T>){
-//             std::uniform_real_distribution<T> dis(min, max);
-//             return dis(RNG::getInstance().gen);
-//         } else {
-//             static_assert(false, "Type must be integral or float-like value.");
-//         }
-//         }
-//     // Generate a lognormal value.
-//     template<typename T = double>
-//     static T lognormal(T const mean, T const stddev) {
-//         static_assert(std::is_arithmetic<T>::value,
-//                       "result_type must be a number type");
-//         std::lognormal_distribution<T> dis(mean, stddev);
-//         return dis(RNG::getInstance().gen);
-//     }
-
-//     // Generate a random normal variable.
-//     template<typename T = double>
-//     static T normal(T const mean, T const stddev) {
-//         static_assert(std::is_arithmetic<T>::value,
-//                       "result_type must be a number type");
-//         std::normal_distribution<T> dis(mean, stddev);
-//         return dis(RNG::getInstance().gen);
-//     }
-
-//     // Generate a random weibull variable.
-//     template<typename T = double>
-//     static T weibull(T const shape, T const scale) {
-//         static_assert(std::is_arithmetic<T>::value,
-//                       "result_type must be a number type");
-//         std::weibull_distribution<T> dis(shape, scale);
-//         return dis(RNG::getInstance().gen);
-//     }
-
-//     // Shuffle a container(vector) in place.
-//     template<typename container>
-//     static void shuffle(container &cont)
-//     {
-//         std::ranges::shuffle(std::begin(cont), std::end(cont), RNG::getInstance().gen);
-//     }
-
-// private:
-//     // Private constructor to prevent instantiation
-//     RNG() : gen(std::random_device()()) {}
-
-//     // Mersenne Twister pseudo-random number generator
-//     std::mt19937 gen{std::random_device()()};
-// };
-
-// //static RNG random = RNG::getInstance();
-
-// #endif
--- source/Math/RNG.cpp
+++ source/Math/RNG.cpp
@@ -2,5 +2,17 @@
 
 namespace Nilib
 {
-    uint64_t RNG::state = 000123;
+    template <size_t stream>
+    uint64_t Nilib::RAN<stream>::state = 00123;
+
+    // Optional: specialize for stream 0, 1, 2
+    template <>
+    uint64_t Nilib::RAN<0>::state = 127;
+    template <>
+    uint64_t Nilib::RAN<1>::state = 0xdead;
+    template <>
+    uint64_t Nilib::RAN<3>::state = 9042582;
+    template <>
+    uint64_t Nilib::RAN<4>::state = 83672389;
+
 } // namespace Nilib
