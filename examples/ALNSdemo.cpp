47cb22592fd3f29fae68752fbfa57834a6d7279c
Niels DESKTOP <niels.dejong@upcmail.nl>
2026-02-24 20:58:32 +0100

    ALNS initial multithreading

--- examples/ALNSdemo.cpp
+++ examples/ALNSdemo.cpp
@@ -59,6 +59,8 @@ int main(int argc, char **argv)
 
     Nilib::ALNS::Params params;
 
+    params.num_threads = 10;
+
     DemoSolution demo(40.0f, 10.0f);
 
     std::vector<Nilib::ALNS::Solver<DemoSolution>::Operator> operators = {op1, op2};
--- include/Nilib/Math/ALNS.hpp
+++ include/Nilib/Math/ALNS.hpp
@@ -11,6 +11,7 @@
 #include <memory>
 #include <functional>
 #include <filesystem>
+#include <thread>
 
 namespace Nilib
 {
@@ -36,6 +37,9 @@ namespace Nilib
 
             // If enabled we log to this file.
             std::filesystem::path history_filename;
+
+            // Threads.
+            size_t num_threads = 1;
         };
 
         // Standard ALNS framework procedure.
@@ -71,19 +75,64 @@ namespace Nilib
                 incumbent = initial;
             }
 
-            // Solve using ALNS.
             void solve()
             {
+                size_t const num_threads = params.num_threads;
+
+                // Print starting information.
+                LOG_PROGRESS("User supplied", d_operators.size(), "operators");
+                LOG_PROGRESS("Optimizing a solution of size ", sizeof(Solution), "bytes");
+                LOG_PROGRESS("Thread count:", num_threads);
+
+                float const best_obj = bestfound.objective();
+                LOG_PROGRESS("Initial solution objective:", best_obj);
+
+                if (num_threads <= 1)
+                {
+                    solve_single_thread();
+                    return;
+                }
+
+                std::mutex global_mutex;
+
+                Solution global_best = bestfound;
+                float global_best_obj = bestfound.objective();
+
+                std::vector<std::thread> threads;
+
+                for (size_t t = 0; t < num_threads; ++t)
+                {
+                    threads.emplace_back(
+                        &Solver<Solution>::worker,
+                        this,
+                        t,
+                        params.max_iterations,
+                        std::ref(global_best),
+                        std::ref(global_best_obj),
+                        std::ref(global_mutex));
+                }
+
+                for (auto &t : threads)
+                    t.join();
+
+                bestfound = global_best;
+                incumbent = global_best;
+
+                LOG_PROGRESS("Best objective:", global_best_obj);
+            }
+
+            // Solve using ALNS.
+            void solve_single_thread()
+            {
+                // Parameter
+                size_t const num_threads = params.num_threads;
+
                 incumbent = bestfound;
 
                 float best_obj = bestfound.objective();
                 float current_obj = best_obj;
                 float temperature = 1.0f;
 
-                // Print starting information.
-                LOG_PROGRESS("User supplied", d_operators.size(), "operators");
-                LOG_PROGRESS("Optimizing a solution of size ", sizeof(Solution), "bytes");
-                LOG_PROGRESS("Initial solution objective:", best_obj);
                 if (params.history_filename != "")
                 {
                     d_logger.reset();
@@ -209,6 +258,112 @@ namespace Nilib
             Callback d_callback_iteration;
 
             constexpr void nullCallback(Solution &solution) {};
+
+            void worker(size_t thread_id,
+                        size_t iterations,
+                        Solution &global_best,
+                        float &global_best_obj,
+                        std::mutex &global_mutex)
+            {
+                // Thread-local copies
+                Solution incumbent_local = bestfound;
+                Solution best_local = bestfound;
+
+                float best_obj = best_local.objective();
+                float current_obj = best_obj;
+                float temperature = 1.0f;
+
+                float const coolingrate =
+                    std::pow(0.01f, 1.0f / iterations);
+
+                bool next_feasible = incumbent_local.feasible();
+                size_t iteration_without_improv = 0;
+
+                Nilib::Timer timer;
+
+                // IMPORTANT: ensure RNG is thread-local
+                // Ideally Nilib::RNG internally uses thread_local
+
+                for (size_t iteration = 0; iteration < iterations; ++iteration)
+                {
+                    if (timer.getNanoseconds() > params.max_duration)
+                        break;
+
+                    if (iteration_without_improv >=
+                        params.max_iterations_without_improvement)
+                        break;
+
+                    Solution previous = incumbent_local;
+
+                    size_t selected_operator_idx =
+                        Nilib::RNG::index(d_operators.size());
+
+                    Operator op = d_operators[selected_operator_idx];
+
+                    bool current_feasible = next_feasible;
+                    op(incumbent_local);
+
+                    float next_obj = incumbent_local.objective();
+                    next_feasible = incumbent_local.feasible();
+
+                    float delta =
+                        (next_obj - current_obj) / current_obj;
+
+                    if (!params.allow_infeasible && !next_feasible)
+                    {
+                        incumbent_local = previous;
+                    }
+                    else
+                    {
+                        bool accept = false;
+
+                        if (delta <= 0)
+                        {
+                            accept = true;
+                        }
+                        else
+                        {
+                            float prob =
+                                std::exp(-5.0f * delta / temperature);
+
+                            if (Nilib::RNG::prob() < prob)
+                                accept = true;
+                        }
+
+                        if (accept)
+                        {
+                            current_obj = next_obj;
+
+                            if (next_obj < best_obj)
+                            {
+                                best_obj = next_obj;
+                                best_local = incumbent_local;
+                                iteration_without_improv = 0;
+                            }
+                        }
+                        else
+                        {
+                            incumbent_local = previous; // correct revert
+                        }
+                    }
+
+                    iteration_without_improv++;
+                    temperature *= coolingrate;
+                }
+
+                // Update global best safely
+                {
+                    std::lock_guard<std::mutex> lock(global_mutex);
+
+                    if (best_obj < global_best_obj)
+                    {
+                        global_best_obj = best_obj;
+                        global_best = best_local;
+                    }
+
+                    LOG_PROGRESS("ALNS solver thread", thread_id, "performed", iterations, "iterations in", timer.getMilliseconds(), "Best objective:", best_obj);
+                }
+            }
         };
 
     } // namespace ALNS
